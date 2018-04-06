c2710b64bff4b9850d6dfa8130574f5c479b456f
Stivali Serna <stivaliserna08@gmail.com>
2018-04-06 13:25:49 -0500

    Create sms-validator functionality

---
+++ encoder-utils.js
@@ -0,0 +1,473 @@
+ /* Apache License Version 2.0, January 2004 http://www.apache.org/licenses/
+
+ TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+
+ Definitions.
+
+ "License" shall mean the terms and conditions for use, reproduction, and
+ distribution as defined by Sections 1 through 9 of this document.
+
+ "Licensor" shall mean the copyright owner or entity authorized by the copyright
+ owner that is granting the License.
+
+ "Legal Entity" shall mean the union of the acting entity and all other entities
+ that control, are controlled by, or are under common control with that entity.
+ For the purposes of this definition, "control" means (i) the power, direct or
+ indirect, to cause the direction or management of such entity, whether by
+ contract or otherwise, or (ii) ownership of fifty percent (50%) or more of the
+ outstanding shares, or (iii) beneficial ownership of such entity.
+
+ "You" (or "Your") shall mean an individual or Legal Entity exercising
+ permissions granted by this License.
+
+ "Source" form shall mean the preferred form for making modifications, including
+ but not limited to software source code, documentation source, and
+ configuration files.
+
+ "Object" form shall mean any form resulting from mechanical transformation or
+ translation of a Source form, including but not limited to compiled object
+ code, generated documentation, and conversions to other media types.
+
+ "Work" shall mean the work of authorship, whether in Source or Object form,
+ made available under the License, as indicated by a copyright notice that is
+ included in or attached to the work (an example is provided in the Appendix
+ below).
+
+ "Derivative Works" shall mean any work, whether in Source or Object form, that
+ is based on (or derived from) the Work and for which the editorial revisions,
+ annotations, elaborations, or other modifications represent, as a whole, an
+ original work of authorship. For the purposes of this License, Derivative Works
+ shall not include works that remain separable from, or merely link (or bind by
+ name) to the interfaces of, the Work and Derivative Works thereof.
+
+ "Contribution" shall mean any work of authorship, including the original
+ version of the Work and any modifications or additions to that Work or
+ Derivative Works thereof, that is intentionally submitted to Licensor for
+ inclusion in the Work by the copyright owner or by an individual or Legal
+ Entity authorized to submit on behalf of the copyright owner. For the purposes
+ of this definition, "submitted" means any form of electronic, verbal, or
+ written communication sent to the Licensor or its representatives, including
+ but not limited to communication on electronic mailing lists, source code
+ control systems, and issue tracking systems that are managed by, or on behalf
+ of, the Licensor for the purpose of discussing and improving the Work, but
+ excluding communication that is conspicuously marked or otherwise designated in
+ writing by the copyright owner as "Not a Contribution."
+
+ "Contributor" shall mean Licensor and any individual or Legal Entity on behalf
+ of whom a Contribution has been received by Licensor and subsequently
+ incorporated within the Work.
+
+ Grant of Copyright License. Subject to the terms and conditions of this
+ License, each Contributor hereby grants to You a perpetual, worldwide,
+ non-exclusive, no-charge, royalty-free, irrevocable copyright license to
+ reproduce, prepare Derivative Works of, publicly display, publicly perform,
+ sublicense, and distribute the Work and such Derivative Works in Source or
+ Object form.
+
+ Grant of Patent License. Subject to the terms and conditions of this License,
+ each Contributor hereby grants to You a perpetual, worldwide, non-exclusive,
+ no-charge, royalty-free, irrevocable (except as stated in this section) patent
+ license to make, have made, use, offer to sell, sell, import, and otherwise
+ transfer the Work, where such license applies only to those patent claims
+ licensable by such Contributor that are necessarily infringed by their
+ Contribution(s) alone or by combination of their Contribution(s) with the Work
+ to which such Contribution(s) was submitted. If You institute patent litigation
+ against any entity (including a cross-claim or counterclaim in a lawsuit)
+ alleging that the Work or a Contribution incorporated within the Work
+ constitutes direct or contributory patent infringement, then any patent
+ licenses granted to You under this License for that Work shall terminate as of
+ the date such litigation is filed.
+
+ Redistribution. You may reproduce and distribute copies of the Work or
+ Derivative Works thereof in any medium, with or without modifications, and in
+ Source or Object form, provided that You meet the following conditions:
+
+ (a) You must give any other recipients of the Work or Derivative Works a copy
+ of this License; and
+
+ (b) You must cause any modified files to carry prominent notices stating that
+ You changed the files; and
+
+ (c) You must retain, in the Source form of any Derivative Works that You
+ distribute, all copyright, patent, trademark, and attribution notices from the
+ Source form of the Work, excluding those notices that do not pertain to any
+ part of the Derivative Works; and
+
+ (d) If the Work includes a "NOTICE" text file as part of its distribution, then
+ any Derivative Works that You distribute must include a readable copy of the
+ attribution notices contained within such NOTICE file, excluding those notices
+ that do not pertain to any part of the Derivative Works, in at least one of the
+ following places: within a NOTICE text file distributed as part of the
+ Derivative Works; within the Source form or documentation, if provided along
+ with the Derivative Works; or, within a display generated by the Derivative
+ Works, if and wherever such third-party notices normally appear. The contents
+ of the NOTICE file are for informational purposes only and do not modify the
+ License. You may add Your own attribution notices within Derivative Works that
+ You distribute, alongside or as an addendum to the NOTICE text from the Work,
+ provided that such additional attribution notices cannot be construed as
+ modifying the License.
+
+ You may add Your own copyright statement to Your modifications and may provide
+ additional or different license terms and conditions for use, reproduction, or
+ distribution of Your modifications, or for any such Derivative Works as a
+ whole, provided Your use, reproduction, and distribution of the Work otherwise
+ complies with the conditions stated in this License.
+
+ Submission of Contributions. Unless You explicitly state otherwise, any
+ Contribution intentionally submitted for inclusion in the Work by You to the
+ Licensor shall be under the terms and conditions of this License, without any
+ additional terms or conditions. Notwithstanding the above, nothing herein shall
+ supersede or modify the terms of any separate license agreement you may have
+ executed with Licensor regarding such Contributions.
+
+ Trademarks. This License does not grant permission to use the trade names,
+ trademarks, service marks, or product names of the Licensor, except as required
+ for reasonable and customary use in describing the origin of the Work and
+ reproducing the content of the NOTICE file.
+
+ Disclaimer of Warranty. Unless required by applicable law or agreed to in
+ writing, Licensor provides the Work (and each Contributor provides its
+ Contributions) on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied, including, without limitation, any warranties
+ or conditions of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
+ PARTICULAR PURPOSE. You are solely responsible for determining the
+ appropriateness of using or redistributing the Work and assume any risks
+ associated with Your exercise of permissions under this License.
+
+ Limitation of Liability. In no event and under no legal theory, whether in tort
+ (including negligence), contract, or otherwise, unless required by applicable
+ law (such as deliberate and grossly negligent acts) or agreed to in writing,
+ shall any Contributor be liable to You for damages, including any direct,
+ indirect, special, incidental, or consequential damages of any character
+ arising as a result of this License or out of the use or inability to use the
+ Work (including but not limited to damages for loss of goodwill, work stoppage,
+ computer failure or malfunction, or any and all other commercial damages or
+ losses), even if such Contributor has been advised of the possibility of such
+ damages.
+
+ Accepting Warranty or Additional Liability. While redistributing the Work or
+ Derivative Works thereof, You may choose to offer, and charge a fee for,
+ acceptance of support, warranty, indemnity, or other liability obligations
+ and/or rights consistent with this License. However, in accepting such
+ obligations, You may act only on Your own behalf and on Your sole
+ responsibility, not on behalf of any other Contributor, and only if You agree
+ to indemnify, defend, and hold each Contributor harmless for any liability
+ incurred by, or claims asserted against, such Contributor by reason of your
+ accepting any such warranty or additional liability.
+
+ END OF TERMS AND CONDITIONS
+
+ APPENDIX: How to apply the Apache License to your work.
+
+ To apply the Apache License to your work, attach the following boilerplate
+ notice, with the fields enclosed by brackets "{}" replaced with your own
+ identifying information. (Don't include the brackets!) The text should be
+ enclosed in the appropriate comment syntax for the file format. We also
+ recommend that a file or class name and description of purpose be included on
+ the same "printed page" as the copyright notice for easier identification
+ within third-party archives. Copyright 2016 Chad Selph
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ this file except in compliance with the License. You may obtain a copy of the
+ License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed
+ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ specific language governing permissions and limitations under the License.
+
+ */
+
+const unicodeToGsm = {
+  0x000a: [0x0a],
+  0x000c: [0x1b, 0x0a],
+  0x000d: [0x0d],
+  0x0020: [0x20],
+  0x0021: [0x21],
+  0x0022: [0x22],
+  0x0023: [0x23],
+  0x0024: [0x02],
+  0x0025: [0x25],
+  0x0026: [0x26],
+  0x0027: [0x27],
+  0x0028: [0x28],
+  0x0029: [0x29],
+  0x002a: [0x2a],
+  0x002b: [0x2b],
+  0x002c: [0x2c],
+  0x002d: [0x2d],
+  0x002e: [0x2e],
+  0x002f: [0x2f],
+  0x0030: [0x30],
+  0x0031: [0x31],
+  0x0032: [0x32],
+  0x0033: [0x33],
+  0x0034: [0x34],
+  0x0035: [0x35],
+  0x0036: [0x36],
+  0x0037: [0x37],
+  0x0038: [0x38],
+  0x0039: [0x39],
+  0x003a: [0x3a],
+  0x003b: [0x3b],
+  0x003c: [0x3c],
+  0x003d: [0x3d],
+  0x003e: [0x3e],
+  0x003f: [0x3f],
+  0x0040: [0x00],
+  0x0041: [0x41],
+  0x0042: [0x42],
+  0x0043: [0x43],
+  0x0044: [0x44],
+  0x0045: [0x45],
+  0x0046: [0x46],
+  0x0047: [0x47],
+  0x0048: [0x48],
+  0x0049: [0x49],
+  0x004a: [0x4a],
+  0x004b: [0x4b],
+  0x004c: [0x4c],
+  0x004d: [0x4d],
+  0x004e: [0x4e],
+  0x004f: [0x4f],
+  0x0050: [0x50],
+  0x0051: [0x51],
+  0x0052: [0x52],
+  0x0053: [0x53],
+  0x0054: [0x54],
+  0x0055: [0x55],
+  0x0056: [0x56],
+  0x0057: [0x57],
+  0x0058: [0x58],
+  0x0059: [0x59],
+  0x005a: [0x5a],
+  0x005b: [0x1b, 0x3c],
+  0x005c: [0x1b, 0x2f],
+  0x005d: [0x1b, 0x3e],
+  0x005e: [0x1b, 0x14],
+  0x005f: [0x11],
+  0x0061: [0x61],
+  0x0062: [0x62],
+  0x0063: [0x63],
+  0x0064: [0x64],
+  0x0065: [0x65],
+  0x0066: [0x66],
+  0x0067: [0x67],
+  0x0068: [0x68],
+  0x0069: [0x69],
+  0x006a: [0x6a],
+  0x006b: [0x6b],
+  0x006c: [0x6c],
+  0x006d: [0x6d],
+  0x006e: [0x6e],
+  0x006f: [0x6f],
+  0x0070: [0x70],
+  0x0071: [0x71],
+  0x0072: [0x72],
+  0x0073: [0x73],
+  0x0074: [0x74],
+  0x0075: [0x75],
+  0x0076: [0x76],
+  0x0077: [0x77],
+  0x0078: [0x78],
+  0x0079: [0x79],
+  0x007a: [0x7a],
+  0x007b: [0x1b, 0x28],
+  0x007c: [0x1b, 0x40],
+  0x007d: [0x1b, 0x29],
+  0x007e: [0x1b, 0x3d],
+  0x00a1: [0x40],
+  0x00a3: [0x01],
+  0x00a4: [0x24],
+  0x00a5: [0x03],
+  0x00a7: [0x5f],
+  0x00bf: [0x60],
+  0x00c4: [0x5b],
+  0x00c5: [0x0e],
+  0x00c6: [0x1c],
+  0x00c9: [0x1f],
+  0x00d1: [0x5d],
+  0x00d6: [0x5c],
+  0x00d8: [0x0b],
+  0x00dc: [0x5e],
+  0x00df: [0x1e],
+  0x00e0: [0x7f],
+  0x00e4: [0x7b],
+  0x00e5: [0x0f],
+  0x00e6: [0x1d],
+  0x00c7: [0x09],
+  0x00e8: [0x04],
+  0x00e9: [0x05],
+  0x00ec: [0x07],
+  0x00f1: [0x7d],
+  0x00f2: [0x08],
+  0x00f6: [0x7c],
+  0x00f8: [0x0c],
+  0x00f9: [0x06],
+  0x00fc: [0x7e],
+  0x0393: [0x13],
+  0x0394: [0x10],
+  0x0398: [0x19],
+  0x039b: [0x14],
+  0x039e: [0x1a],
+  0x03a0: [0x16],
+  0x03a3: [0x18],
+  0x03a6: [0x12],
+  0x03a8: [0x17],
+  0x03a9: [0x15],
+  0x20ac: [0x1b, 0x65],
+};
+
+const util = {
+  map: function(sub, func) {
+    return [].map.apply(sub, [func]);
+  },
+  concatMap: function(sub, func) {
+    return [].concat.apply([], util.map(sub, func));
+  },
+  id: function(x) {
+    return x;
+  },
+  isHighSurrogate: function(c) {
+    let codeUnit = c.charCodeAt != undefined ? c.charCodeAt(0) : c;
+    return codeUnit >= 0xd800 && codeUnit <= 0xdbff;
+  },
+
+  /**
+   * Take a string and return a list of the unicode characters
+   *
+   * @param {string} string
+   * @return {*}
+  */
+  unicodeCharacters: function(string) {
+    let chars = util.map(string, util.id);
+    let result = [];
+    while (chars.length > 0) {
+      if (util.isHighSurrogate(chars[0])) {
+        result.push(chars.shift() + chars.shift());
+      } else {
+        result.push(chars.shift());
+      }
+    }
+    return result;
+  },
+  /**
+   * Take a string and return a list of the unicode codepoints
+   *
+   * @param {string} string
+   * @return {*}
+  */
+  unicodeCodePoints: function(string) {
+    let charCodes = util.map(string, function(x) {
+      return x.charCodeAt(0);
+    });
+    let result = [];
+    while (charCodes.length > 0) {
+      if (util.isHighSurrogate(charCodes[0])) {
+        let high = charCodes.shift();
+        let low = charCodes.shift();
+        result.push((high - 0xd800) * 0x400 + (low - 0xdc00) + 0x10000);
+      } else {
+        result.push(charCodes.shift());
+      }
+    }
+    return result;
+  },
+  /**
+   * Encode a single (unicode) character into UTF16 "bytes"
+   * A single unicode character may be 2 javascript characters
+   *
+   * @param {string} char
+   * @return {*}
+  */
+  encodeCharUtf16: function(char) {
+    return util.concatMap(char, function(c) {
+      return [(0xff00 & c.charCodeAt(0)) >> 8, 0x00ff & c.charCodeAt(0)];
+    });
+  },
+  /**
+   * Encode a single character into GSM0338 "bytes"
+   *
+   * @param {string} char
+   * @return {*}
+  */
+  encodeCharGsm: function(char) {
+    return unicodeToGsm[char.charCodeAt(0)];
+  },
+
+  /**
+   * Encode each with
+   *
+   * @param {Function} doEncode
+   * @return {*}
+  */
+  _encodeEachWith: function(doEncode) {
+    return function(s) {
+      return util.map(util.unicodeCharacters(s), doEncode);
+    };
+  },
+
+  /**
+   * Pick encoding
+   *
+   * @param {string} s
+   * @return {*}
+  */
+  pickencoding: function(s) {
+    // choose gsm if possible otherwise ucs2
+    if (
+      util.unicodeCodePoints(s).every(function(x) {
+        return x in unicodeToGsm;
+      })
+    ) {
+      return 'gsm';
+    } else {
+      return 'ucs2';
+    }
+  },
+
+  /**
+   * Segment with
+   *
+   * @param {number} maxSingleSegmentSize
+   * @param {number} maxConcatSegmentSize
+   * @param {Function} doEncode
+   * @return {*}
+  */
+  _segmentWith: function(maxSingleSegmentSize, maxConcatSegmentSize, doEncode) {
+    return function(listOfUnichrs) {
+      let bytes = util.map(listOfUnichrs, doEncode);
+
+      if (listOfUnichrs.length == 0) {
+        return [];
+      } else if ([].concat.apply([], bytes).length <= maxSingleSegmentSize) {
+        return [{text: listOfUnichrs, bytes: bytes}];
+      }
+
+      let segments = [];
+      while (listOfUnichrs.length > 0) {
+        let segment = {text: [], bytes: []};
+        let length = 0;
+        function nextChrLen() {
+          return bytes[0] === undefined ? length : length + bytes[0].length;
+        }
+        while (
+          listOfUnichrs.length > 0 &&
+          nextChrLen() <= maxConcatSegmentSize
+        ) {
+          let c = listOfUnichrs.shift();
+          let b = bytes.shift();
+          segment.text.push(c);
+          segment.bytes.push(b);
+          if (b != undefined) length += b.length;
+        }
+        segments.push(segment);
+      }
+      return segments;
+    };
+  },
+};
+
+window.encoderUtils = util;
